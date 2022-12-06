b4e3e1e3f068bc458d108417be0231f855f846c1
Tóth Keve András <toth.keve2@gmail.com>
2022-12-06 19:38:33 +0100

    Stripe payment added

---
+++ WebShopServer/src/main/java/com/example/WebShopServer/Models/StripeClient.java
@@ -0,0 +1,27 @@
+package com.example.WebShopServer.Models;
+
+import com.stripe.Stripe;
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class StripeClient {
+
+    @Autowired
+    StripeClient() {
+        Stripe.apiKey = "sk_test_XXXXXXXXXXXXXXXXX";
+    }
+
+    public Charge chargeCreditCard(String token, double amount) throws Exception {
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", (int)(amount * 100));
+        chargeParams.put("currency", "HUF");
+        chargeParams.put("source", token);
+        Charge charge = Charge.create(chargeParams);
+        return charge;
+    }
+}
